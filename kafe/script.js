b686d2c52948e6a7b2eb1dfcba64eabd831bfc80
hakangrdl <hakangurdal1907@gmail.com>
2025-01-27 20:58:04 +0300

    Menü

---
+++ kafe/script.js
@@ -0,0 +1,158 @@
+const girisButtons = document.querySelectorAll('.giris');
+const imageContainers = document.querySelectorAll('.image-container');
+
+// Butonlar için kategori değiştirme işlevi
+girisButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    changeImageCategory(e.target.id);
+  });
+});
+
+// Resimler için tıklama işlevi
+imageContainers.forEach((container) => {
+  const image = container.querySelector('.image');
+  const imageInfo = container.querySelector('.image-info');
+
+  image.addEventListener('click', () => {
+    if (image.classList.contains('active')) {
+      // Tıklanan resim aktifse, pasif yap ve açıklamayı göster
+      image.classList.remove('active');
+      image.classList.add('inactive');
+      imageInfo.style.display = 'block';
+    } else {
+      // Tüm resimleri pasifleştir ve açıklamaları göster
+      imageContainers.forEach((c) => {
+        const otherImage = c.querySelector('.image');
+        const otherInfo = c.querySelector('.image-info');
+        otherImage.classList.remove('active');
+        otherImage.classList.add('inactive');
+        otherInfo.style.display = 'block'; // Açıklamaları göster
+      });
+
+      // Tıklanan resmi aktif hale getir ve açıklamasını gizle
+      image.classList.remove('inactive');
+      image.classList.add('active');
+      imageInfo.style.display = 'none'; // Açıklamayı gizle
+    }
+
+    // Opaklıkları güncelle
+    updateImageOpacity();
+  });
+});
+
+// Opaklıkları güncelleme işlevi
+function updateImageOpacity() {
+  imageContainers.forEach((container) => {
+    const image = container.querySelector('.image');
+    if (image.classList.contains('active')) {
+      image.style.opacity = '1'; // Aktif resim tam görünür
+    } else {
+      image.style.opacity = '0.4'; // Pasif resimler yarı görünür
+    }
+  });
+}
+
+// Kategori değiştirildiğinde galeri içeriğini güncelle
+function changeImageCategory(category) {
+  let imageArray = [];
+
+  if (category === 'specialButton') {
+    imageArray = [
+      {
+        src: 'https://globalassets.starbucks.com/digitalassets/products/bev/SBX20211210_MochaCookieCrumbleFrapp.jpg?impolicy=1by1_wide_topcrop_630',
+        name: 'Cookie Mocha',
+        price: '120/140 ₺',
+      },
+      {
+        src: 'https://img.freepik.com/premium-photo/caramel-popcorn-latte-cinema-themed-cafe-with-blurred-lights-background_887552-46847.jpg',
+        name: 'Popcorn Latte',
+        price: '120/140 ₺',
+      },
+      {
+        src: 'https://www.einfachbacken.de/sites/einfachbacken.de/files/styles/full_width_tablet_4_3/public/2022-12/pumpkin_spice_latte_1.jpeg?h=93d8d2a6&itok=mIl5Ehs8',
+        name: 'Pumpkin Spice Latte',
+        price: '120/140 ₺',
+      },
+      {
+        src: 'https://shottbeverages.com/wp-content/uploads/2019/10/Salted-Caramel-Coffee-1.jpg',
+        name: 'Salted Caramel',
+        price: '120/140 ₺',
+      },
+      { src: 'https://www.cariboucoffee.com.tr/upload/prod/cc-berry-white-mocha.png', name: 'Berry White', price: '120/140 ₺' },
+      { src: 'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/58D395A1-590A-4ED3-A810-16439E2EC175/Derivates/45b2c006-2c44-491f-8b68-f88a1d6a223e.jpg', name: 'Toffe Nut Latte', price: '120/140 ₺' },
+      { src: 'https://images.ctfassets.net/v601h1fyjgba/7HTOK351rMiQkGqcem5wUl/1d26827e666fee77bc09bdeae3a6d708/Caramel_Cookie_Coffee.jpg', name: 'Cookie Caramel', price: '120/140 ₺' },
+      { src: 'https://www.puratos.us/content/dam/usa/images/recipes/patisserie/hispanic/Almond%20Coffee.jpeg/jcr:content/renditions/cq5dam.web.800.800.webp', name: 'ALMOUND Macchiato', price: '120/140 ₺' },
+    ];
+  } else if (category === 'sicaklarButton') {
+    imageArray = [
+      {
+        src: 'https://www.shutterstock.com/image-photo/espresso-coffee-transparent-cup-600nw-2512810803.jpg',
+        name: 'Expresso',
+        price: '65/85 ₺',
+      },
+      {
+        src: 'https://assets.beanbox.com/blog_images/AB7ud4YSE6nmOX0iGlgA.jpeg',
+        name: 'Americano',
+        price: '80/90 ₺',
+      },
+      {
+        src: 'https://bikafein.com/wp-content/uploads/2022/08/CAH_Header_Caramel_macch_2880x1660-2-scaled.jpg',
+        name: 'Caramel Macchiato',
+        price: '65/75 ₺',
+      },
+      {
+        src: 'https://st.depositphotos.com/2481033/3144/i/450/depositphotos_31444643-stock-photo-cup-of-coffee-and-coffee.jpg',
+        name: 'Filtre Kahve',
+        price: '70/80 ₺',
+      },
+      { src: 'https://media.istockphoto.com/id/1152767411/tr/foto%C4%9Fraf/bardak-kahve-latte-k%C4%B1rpma-yolu-ile-beyaz-arka-planda-izole.jpg?s=612x612&w=0&k=20&c=xrDDgfFul8T6U0bShSPzSFJVDNekFJJdaJU7JRnVHdw=', name: 'Latte', price: '90/105 ₺' },
+      { src: 'https://www.cuisinart.com/dw/image/v2/ABAF_PRD/on/demandware.static/-/Sites-us-cuisinart-sfra-Library/default/dw30047d66/images/recipe-Images/cappuccino1-recipe.jpg?sw=1200&sh=1200&sm=fit', name: 'Cappuccino', price: '90/105 ₺' },
+      { src: 'https://downshiftology.com/wp-content/uploads/2023/11/Mexican-Hot-Chocolate-main-500x500.jpg', name: 'Hot Chocolate', price: '100/115 ₺' },
+      { src: 'https://odulbufe.com/wp-content/uploads/2021/08/Sahlep.jpg', name: 'Sahlep', price: '100/115 ₺' },
+    ];
+  } else if (category === 'icecekButton') {
+    imageArray = [
+      { src: 'icecek1.jpg', name: 'İçecek 1', price: '₺20' },
+      { src: 'icecek2.jpg', name: 'İçecek 2', price: '₺25' },
+      { src: 'icecek3.jpg', name: 'İçecek 3', price: '₺30' },
+      { src: 'icecek4.jpg', name: 'İçecek 4', price: '₺35' },
+      { src: 'icecek1.jpg', name: 'İçecek 1', price: '₺20' },
+      { src: 'icecek2.jpg', name: 'İçecek 2', price: '₺25' },
+      { src: 'icecek3.jpg', name: 'İçecek 3', price: '₺30' },
+      { src: 'icecek4.jpg', name: 'İçecek 4', price: '₺35' },
+    ];
+  }
+
+  updateImageGallery(imageArray);
+}
+
+// Galeri içeriğini güncelle
+function updateImageGallery(imageArray) {
+  imageContainers.forEach((container, index) => {
+    const image = container.querySelector('.image');
+    const imageInfo = container.querySelector('.image-info');
+    const name = container.querySelector('h4');
+    const price = container.querySelector('p');
+
+    if (index < imageArray.length) {
+      image.style.backgroundImage = `url('${imageArray[index].src}')`;
+      name.textContent = imageArray[index].name;
+      price.textContent = imageArray[index].price;
+
+      // Görünürlüğü aktif yap
+      image.classList.remove('inactive');
+      image.classList.remove('active');
+      imageInfo.style.display = 'block';
+    } else {
+      // Fazla resimleri gizle
+      image.style.backgroundImage = 'none';
+      name.textContent = '';
+      price.textContent = '';
+      image.classList.add('inactive');
+      imageInfo.style.display = 'none';
+    }
+  });
+
+  // Opaklıkları güncelle
+  updateImageOpacity();
+}
